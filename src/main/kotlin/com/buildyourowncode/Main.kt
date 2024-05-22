5025d51f1cd739efb2f4da9c5498587bbf3ab117
Ryan Mayobre <rmayobre@pm.me>
2024-05-22 14:06:49 -0400

    Initial commit for kotlin code

---
+++ src/main/kotlin/com/buildyourowncode/Main.kt
@@ -0,0 +1,5 @@
+package com.buildyourowncode
+
+fun main() {
+    println("Hello World!")
+}
---
+++ src/test/kotlin/com/buildyourowncode/MainTest.kt
@@ -0,0 +1,11 @@
+package com.buildyourowncode
+
+import org.junit.jupiter.api.Test
+
+class MainTest {
+
+    @Test
+    fun `test main function`() {
+        main()
+    }
+}
